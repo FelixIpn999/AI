bed7a65e92a9b8cc9fcfd7b1dccf2d77d8605a2f
FelixIpn999 <edgeratedrsuperstar999@hotmail.com>
2017-05-06 22:48:19 -0500

    Update regressor

--- regressor.py
+++ regressor.py
@@ -2,6 +2,7 @@ import sys
 
 import numpy as np
 
+import sklearn.metrics as sm
 filename = sys.argv[1]
 X = []
 y = []
@@ -50,3 +51,17 @@ plt.plot(X_test, y_test_pred, color='black', linewidth=4)
 plt.xticks(())
 plt.yticks(())
 plt.show()
+
+print ("Mean absolute error =", round(sm.mean_absolute_error(y_test,
+y_test_pred), 2))
+
+print("Mean squared error =", round(sm.mean_squared_error(y_test,
+y_test_pred), 2))
+
+print ( "Median absolute error =",
+round(sm.median_absolute_error(y_test, y_test_pred), 2))
+
+print ("Explained variance score =",
+round(sm.explained_variance_score(y_test, y_test_pred), 2))
+
+print ("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
